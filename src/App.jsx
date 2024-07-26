1c42bf2134c0e1539611c6ba829135e5ea52f80a
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2024-07-26 16:58:50 -0300

    chore(app): Organize component imports and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import ActionSection from "./components/ActionSection";
+import LogoSection from "./components/LogoSection";
+import MainSide from "./components/MainSide";
+import Nav from "./components/Nav";
+import NavIcon from "./components/NavIcon";
 import ScreenBase from "./components/ScreenBase";
+import SideBar from "./components/SideBar";
 
 const App = () => {
   return (
-    <ScreenBase>App</ScreenBase>
+    <ScreenBase>
+      <SideBar>
+        <LogoSection />
+        <Nav>
+          <NavIcon icon="package" onClick={() => { console.log('button 1') }} />
+          <NavIcon icon="package" onClick={() => { console.log('button 2') }} />
+          <NavIcon icon="package" onClick={() => { console.log('button 3') }} />
+          <NavIcon icon="package" onClick={() => { console.log('button 4') }} />
+        </Nav>
+      </SideBar>
+      <MainSide>
+        <ActionSection icon='stocks' heading='Cadastro de Materiais'>
+          <button>+</button>
+        </ActionSection>
+      </MainSide>
+    </ScreenBase>
   )
 }
 
